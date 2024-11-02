51bfb6614ece12f3a468ae412b1c35582725ce44
LutfullaSamigullayev <Qwerty.sayt@gmail.com>
2024-11-02 17:17:03 +0500

    complate price

--- src/components/price/components/Card.tsx
+++ src/components/price/components/Card.tsx
@@ -2,11 +2,13 @@ import { PriceItems } from "@/types";
 import { useTranslations } from "next-intl";
 
 export const PriceCard = ({ title, price }: PriceItems) => {
-    const t = useTranslations("Price");
+  const t = useTranslations("Price");
   return (
-    <div className="w-full px-2 flex justify-between border-b border-orange">
+    <div className="w-full px-2 flex items-end justify-between border-b border-orange">
       <p className="">{title}</p>
-      <span className="w-fit pl-4">{price} {t("money")}</span>
+      <span className="w-fit whitespace-nowrap pl-4">
+        {price} {t("money")}
+      </span>
     </div>
   );
 };
--- src/components/price/index.tsx
+++ src/components/price/index.tsx
@@ -1,7 +1,7 @@
-
 import { PriceItems } from "@/types";
 import { useTranslations } from "next-intl";
 import { PriceCard } from "./components";
+import { ButtonBron } from "../buttonBron";
 
 export const Price = () => {
   const t = useTranslations("Price");
@@ -27,20 +27,19 @@ export const Price = () => {
       title: t("Haircut and Facial"),
       price: 100,
     },
-    
   ];
 
-    return (
-        <section id="price" className="containerUz section">
-            <div className="flex flex-col items-center">
-                <h1 className="title">{t("title")}</h1>
-                <div className="grid justify-center grid-cols-1 sm:grid-cols-2 gap-4">
-                {priceItems.map((item) => (
-                    <PriceCard key={item.id} {...item} />
-                ))}
-
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="price" className="containerUz section">
+      <div className="flex flex-col items-center">
+        <h1 className="title">{t("title")}</h1>
+        <div className="grid justify-center grid-cols-1 sm:grid-cols-2 py-4 gap-4">
+          {priceItems.map((item) => (
+            <PriceCard key={item.id} {...item} />
+          ))}
+        </div>
+        <ButtonBron />
+      </div>
+    </section>
+  );
+};
